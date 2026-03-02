47a2a028567850e1d1d55bd5030d0cfe9c30d8b0
gtapiero <giovanny.tapiero@pi.edu.co>
2026-03-02 16:21:28 -0500

    commit - SerieTresService / .

--- src/service/SerieTresService.java
+++ src/service/SerieTresService.java
@@ -1,4 +1,21 @@
 package service;
 
+import dao.SerieTresDAO;
+
 public class SerieTresService {
-}
+
+    private final SerieTresDAO dao = new SerieTresDAO();
+
+    public int calcularSuma(int limite) {
+        int suma = 0;
+        for (int i = 3; i <= limite; i += 3) {
+            suma += i;
+        }
+        return suma;
+    }
+
+    public void procesarSerie(int limite) throws Exception {
+        int resultado = calcularSuma(limite);
+        dao.guardarResultado(limite, resultado);
+    }
+}   
